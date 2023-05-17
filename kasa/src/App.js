f9695ef2e3fb69515ea7663cb36fa43f7a8027ed
Assia-B00 <assia.bouchlaghem@outlook.com>
2023-05-17 14:01:35 +0200

    ok

---
+++ kasa/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Chez vous, partout et ailleurs</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kasa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
